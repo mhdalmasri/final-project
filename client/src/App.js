384764e0ebacf004354c1634033821e84212fc4a
nayif03 <naif.nayoul@yahoo.com>
2019-09-02 10:17:30 +0200

    create a router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.scss";
 
 import Landing from "./pages/Landing";
+import Homepage from "./pages/Homepage";
+import Footer from './components/Footer'
+import MyToys from "./pages/MyToys";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+       <Route path="/" exact component={Landing} />
+        <Route path="/homepage/" component={Homepage} />
+        <Route path="/myToys/" component={MyToys} />
+    </Router>
   );
 }
 
